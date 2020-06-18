4c7a33de6e8c607a6d3244f49487bfb248511abe
mfikri83 <fikrivfx789@gmail.com>
2020-06-18 15:05:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea ab, nostrum officiis magnam reiciendis veritatis esse eius voluptatem harum error officia sequi culpa non voluptatibus. Aspernatur dolorum ullam unde omnis.</p>
+    <ul>
+        <li>Dika</li>
+        <li>Ikiii</li>
+    </ul>
 </body>
 </html>
